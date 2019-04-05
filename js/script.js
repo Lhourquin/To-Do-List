ebc82b9cf72ed26084d2fc3afbe707c306d9394a
Lhourquin <lhourquin@gmail.com>
2019-04-06 00:59:55 +0200

    add all script js and replace the function onclick for submit li to the shopping list

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+let input = document.getElementsByTagName("input")[0];
+let ul = document.getElementsByTagName("ul")[0];
+let submit = document.getElementById("submit");
+
+function addLiContent() {
+  if(input.value == ""){
+    return null;
+  }else{
+    ul.insertAdjacentHTML('beforeend', "<li>" + input.value + "</li>");
+    deleteLiContent();
+  }
+}
+
+function deleteLiContent(){
+
+  input.value = "";
+}
+
+submit.onclick = addLiContent;
+
+input.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    submit.click();
+  }
+});
+
+ul.onclick = function (event){
+   event.target.classList.toggle("checked");
+}; 
