969f9e92e4d17ca006d10e284ada2a839dfac800
lhourquin <lhourquin@gmail.com>
2020-03-10 09:56:38 +0100

    localtorage ok

--- js/script.js
+++ js/script.js
@@ -2,23 +2,29 @@
 let input = document.getElementById("input");
 let ul = document.getElementsByTagName("ul")[0];
 let submit = document.getElementById("submit");
+let arr = ul.children;
 
+/////function for add li to the list   and to the locale storage
 function addLiContent() {
 
   if(input.value == ""){
     return null;
   }else{
-    ul.insertAdjacentHTML('afterbegin', "<li >" + input.value + "<div class=''><i class='fas fa-trash poubelle'></i></div>" + "</li>" );
+    ul.insertAdjacentHTML('afterbegin', "<li>" + input.value + "<div class=''><i class='fas fa-trash poubelle'></i></div>" + "</li>" );
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+      
+      newArr.push(arr[i].innerHTML);
+      window.localStorage.list = newArr;
+
+    }
+    console.log(arr);
     deleteLiContent();
   }
 }
 
-function deleteLiContent(){
-
-  input.value = "";
-}
-
 submit.onclick = addLiContent;
+
 
 input.addEventListener("keyup", function(event) {
   event.preventDefault();
@@ -26,6 +32,14 @@ input.addEventListener("keyup", function(event) {
     submit.click();
   }
 });
+
+
+///////Function for reset the input value , checkeed and remove
+
+function deleteLiContent(){
+
+  input.value = "";
+}
 
 ul.onclick = function (event){
   let target = event.target;
